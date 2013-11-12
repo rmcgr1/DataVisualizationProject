e4f414c98ff906140d8808d8c193b0b1182f57c0
rmcgr1 <rmcgr1@umbc.edu>
2013-11-11 19:52:18 -0500

    Added ability to put different data against x and y axis' numerator and denominator

--- visualization.js
+++ visualization.js
@@ -14,6 +14,7 @@ var color = d3.scale.category20();
 
 //Test datasets
 //TODO: have this loaded from outside static files?
+//TODO: does not like zero values
 
 var FTE = [
 	   [15, "Afric Std"], 
@@ -27,7 +28,7 @@ var FT_FAC = [
 	      [2, "Afric Std"],
 	      [9, "Dance"],
 	      [12, "Amer Std"],
-	      [0, "CMSC"]
+	      [4, "CMSC"]
 	      ];
 
 var PT_FAC = [
@@ -37,10 +38,19 @@ var PT_FAC = [
 	      [10, "CMSC"]
 	      ];
 
-var xData = FT_FAC;
-var yData = FTE;
-var xData_label = "Full Time Faculty";
-var yData_label = "Full Time Equivalent Student";
+//Data variables - these need to match with what is checked by default in the forms
+var xData_numerator = FT_FAC;
+var xData_denominator = FT_FAC;
+var yData_numerator = FTE;
+var yData_denominator = FTE;
+
+var xData = get_ratio_values(xData_numerator, xData_denominator);
+var yData = get_ratio_values(yData_numerator, yData_denominator);
+
+var xData_label_numerator = "Full Time Faculty";
+var xData_label_denominator = "Full Time Faculty";
+var yData_label_numerator = "Full Time Equivalent Student";
+var yData_label_denominator = "Full Time Equivalent Student";
 
 //Set Scales and Axis
 //TODO: get max of each dataset
@@ -88,6 +98,7 @@ var tooltip = d3.select("body")
 var min = d3.min(get_values(xData));
 var max = d3.max(get_values(xData));
 
+//Bulid each datapoint
 svg.selectAll("circle")
     .data(xData)
     .enter()
@@ -106,13 +117,14 @@ svg.selectAll("circle")
 	    return scale_radius(d[0],min,max);
 	})
     .on("mouseover", function(d,i){
-	    tooltip.text(d[1] + "\n" + xData_label + ": " + d[0] + "\n" + yData_label+ ": " + yData[i][0]);
+	    tooltip.text(d[1] + "\n" + xData_label_numerator + "/" + xData_label_denominator +": " + (d[0]).toFixed(3) + "\n" + yData_label_numerator + "/" + yData_label_denominator + ": " + (yData[i][0]).toFixed(3));
 	    return tooltip.style("visibility", "visible");})
     .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
     .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
 
-//Old tooltip    .append("svg:title")
+//Old tooltip    
+//    .append("svg:title")
 //    .text(function(d,i){ return d[1] + "\n" + xData_label + ": " + d[0] + "\n" + yData_label+ ": " + yData[i][0]});
     
 //Create X axis
@@ -130,29 +142,52 @@ svg.append("g")
 
 d3.selectAll("input").on("change", function change() {
 	
-	if(this.value === "fte_xaxis"){
-	    xData = FTE;
-	    xData_label = "Full Time Equivalent Student";
-	} else if (this.value === "ft_fac_xaxis"){
-	    xData = FT_FAC;
-	    xData_label = "Full Time Faculty";
-	} else if (this.value === "pt_fac_xaxis"){
-	    xData = PT_FAC;
-	    xData_label = "Part Time Faculty";
+	if(this.value === "fte_xaxis_numerator"){
+	    xData_numerator = FTE;
+	    xData_label_numerator = "Full Time Equivalent Student";
+	} else if(this.value === "fte_xaxis_denominator"){
+	    xData_denominator = FTE;
+	    xData_label_denominator = "Full Time Equivalent Student";
+	} else if (this.value === "ft_fac_xaxis_numerator"){
+	    xData_numerator = FT_FAC;
+	    xData_label_numerator = "Full Time Faculty";
+	} else if (this.value === "ft_fac_xaxis_denominator"){
+	    xData_denominator = FT_FAC;
+	    xData_label_denominator = "Full Time Faculty";
+	} else if (this.value === "pt_fac_xaxis_numerator"){
+	    xData_numerator = PT_FAC;
+	    xData_label_numerator = "Part Time Faculty";
+	}else if (this.value === "pt_fac_xaxis_denominator"){
+	    xData_denominator = PT_FAC;
+	    xData_label_denominator = "Part Time Faculty";
 	}
-	else if (this.value === "fte_yaxis"){
-	    yData = FTE;
-	    yData_label = "Full Time Equivalent Student";
+	else if (this.value === "fte_yaxis_numerator"){
+	    yData_numerator = FTE;
+	    yData_label_numerator = "Full Time Equivalent Student";
+	}else if (this.value === "fte_yaxis_denominator"){
+	    yData_denominator = FTE;
+	    yData_label_denominator = "Full Time Equivalent Student";
 	}
-	else if (this.value === "ft_fac_yaxis"){
-	    yData = FT_FAC;
-	    yData_label = "Full Time Faculty";
-	}
-	else if (this.value === "pt_fac_yaxis"){
-	    yData = PT_FAC;
-	    yData_label = "Part Time Faculty";
+	else if (this.value === "ft_fac_yaxis_numerator"){
+	    yData_numerator = FT_FAC;
+	    yData_label_numerator = "Full Time Faculty";
+	}else if (this.value === "ft_fac_yaxis_denominator"){
+	    yData_denominator = FT_FAC;
+	    yData_label_denominator = "Full Time Faculty";
+	}    
+	else if (this.value === "pt_fac_yaxis_numerator"){
+	    yData_numerator = PT_FAC;
+	    yData_label_numerator = "Part Time Faculty";
+	}else if (this.value === "pt_fac_yaxis_denominator"){
+	    yData_denominator = PT_FAC;
+	    yData_label_denominator = "Part Time Faculty";
 	}
 	
+	//Now that the ratios have changed, set new xData and yData
+	xData = get_ratio_values(xData_numerator, xData_denominator);
+	yData = get_ratio_values(yData_numerator, yData_denominator);
+
+
 	//Resize range for plotting new values and each axis
 	xScale.domain([0, d3.max(get_values(xData))])
 	    .range([padding, w - padding]);
@@ -178,8 +213,6 @@ d3.selectAll("input").on("change", function change() {
 	    .attr("r", function(d){
 		    return scale_radius(d[0],min,max);
 		});
-
-	
 	
 	svg.select(".x-axis")
 	    .transition()
@@ -198,9 +231,28 @@ d3.selectAll("input").on("change", function change() {
 //////
 
 function scale_radius(val, min, max){
+    if(min == max){
+	console.log("Warning: scaling radius with min value == max value, setting max = min * 2");
+	max = min * 2;
+    }
     return radius_min + (radius_max - radius_min) * (val - min)/(max - min);
 }
 
+function get_ratio_values(arr1, arr2){
+    ratio_arr = [];
+    for (var i = 0; i < arr1.length; i++){
+	if(arr1[i][0] == 0){
+	    console.log("Warning: zero value in ratio  - replacing with .01");
+	    arr1[i][0] = .01;
+	} else if(arr2[i][0] == 0){
+	    console.log("Warning: zero value in ratio - replacing with .01");
+	    arr2[i][0] = .01;
+	}
+	ratio_arr.push([(arr1[i][0] / arr2[i][0]), arr1[i][1]]);	
+    }
+    console.log(ratio_arr);
+    return ratio_arr;
+}
 
 function get_values(arr){
     var val_array = [];
@@ -219,7 +271,6 @@ function color_by_department(department){
     } else if (department === "Amer Std"){
 	return color(2);
     } else if (department === "CMSC"){
-	console.log(color(29));
-	return color(29);
+	return color(3);
     }
 }
